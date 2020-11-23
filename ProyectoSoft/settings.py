620ae83c9e9f0dba5e5fd1fb46cd73f70c0122bf
Kevin <kevin.2cardenas@gmail.com>
2020-11-23 15:13:06 -0600

    All ready

--- ProyectoSoft/settings.py
+++ ProyectoSoft/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ProjectApp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ProyectoSoft.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'SoftwareP',
+        'USER': 'postgres',
+        'PASSWORD': '12',
+        'HOTS': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
 
