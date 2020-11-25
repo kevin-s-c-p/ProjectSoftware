06b40241748d745c583665e8f855de58f47683ed
Kevin <kevin.2cardenas@gmail.com>
2020-11-24 18:21:03 -0600

    Login ready

--- ProjectApp/urls.py
+++ ProjectApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib.auth import login, logout
 
 from ProjectApp import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
+    path('',views.login_user, name='login'),
     path('index/',views.index, name='index'),
 ]
--- ProjectApp/views.py
+++ ProjectApp/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
-def home(request):
+def login_user(request):
+    message = "Welcome"
 
-    return render(request,"ProjectApp/login.html")
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('login')
+        
+        else:
+            message = "Username or Password incorrect"
+
+
+    return render(request,"ProjectApp/login.html",{'message':message})
 
 
 def index(request):
