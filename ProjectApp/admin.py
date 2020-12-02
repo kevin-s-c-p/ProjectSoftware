b85def5955f6871eaac606e9f506080f7548c989
Kevin <kevin.2cardenas@gmail.com>
2020-12-01 21:41:43 -0600

    add files in admin ready

--- ProjectApp/admin.py
+++ ProjectApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import archivos
 
 # Register your models here.
+
+class archivosAdmin(admin.ModelAdmin):
+    readonly_fields = ('fecha',)
+
+admin.site.register(archivos,archivosAdmin)
